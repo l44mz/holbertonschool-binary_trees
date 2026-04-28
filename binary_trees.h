1db18863f014b88abe43517fbcd0afd6a3edc032
janaalhazmi <janaalhazmi78gmail.com>
2026-04-28 11:28:00 +0000

    Add : task 4 is leaf file : 4-binary_tree_is_leaf.c

--- binary_trees.h
+++ binary_trees.h
@@ -27,7 +27,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 void binary_tree_delete(binary_tree_t *tree); // 3
-
+int binary_tree_is_leaf(const binary_tree_t *node); // 4
 int binary_tree_is_root(const binary_tree_t *node); // 5
 
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int)); // 7
