e11661a6683a03a360a766d8dd92663acd811252
Lama <lamamqqz@gmail.com>
2026-04-29 15:44:06 +0300

    fix: duplicate symbol issue

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,20 +1,20 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - Measures the height of a binary tree
+ * tree_height - Measures the height of a binary tree
  * @tree: Pointer to the root node of the tree to measure
  *
  * Return: Height of the tree, or 0 if tree is NULL
  */
-size_t binary_tree_height(const binary_tree_t *tree)
+static size_t tree_height(const binary_tree_t *tree)
 {
 	size_t left_height, right_height;
 
 	if (tree == NULL)
 		return (0);
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+	left_height = tree_height(tree->left);
+	right_height = tree_height(tree->right);
 
 	if (left_height > right_height)
 		return (1 + left_height);
@@ -32,6 +32,6 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	return ((int)binary_tree_height(tree->left) -
-		(int)binary_tree_height(tree->right));
+	return ((int)tree_height(tree->left) -
+		(int)tree_height(tree->right));
 }
