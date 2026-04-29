686531651d235cc01cd0db31af04452b3e72f65e
Lama <lamamqqz@gmail.com>
2026-04-29 15:39:48 +0300

    fix: code structure to compile correctly

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,27 +1,6 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - Measures the height of a binary tree
- * @tree: Pointer to the root node of the tree to measure
- *
- * Return: Height of the tree, or 0 if tree is NULL
- */
-static size_t binary_tree_height(const binary_tree_t *tree)
-{
-	size_t left_height, right_height;
-
-	if (tree == NULL)
-		return (0);
-
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-
-	if (left_height > right_height)
-		return (1 + left_height);
-	return (1 + right_height);
-}
-
-/**
  * binary_tree_balance - Measures the balance factor of a binary tree
  * @tree: Pointer to the root node of the tree to measure
  *
