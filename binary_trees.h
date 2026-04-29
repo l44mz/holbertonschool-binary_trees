cecad2fc344bddcf07aea0a5673d1e9584145e73
Lama <lamamqqz@gmail.com>
2026-04-29 15:48:26 +0300

    add: task 18: finds the uncle of a node

--- binary_trees.h
+++ binary_trees.h
@@ -43,4 +43,6 @@ int binary_tree_balance(const binary_tree_t *tree);
 
 int binary_tree_is_perfect(const binary_tree_t *tree);
 
+binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+
 #endif /* BINARY_TREES_H */
